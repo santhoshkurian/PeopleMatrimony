015781831e1666a3a8815c10cc28773d032cf17e
santhosh kurian <santhoshkurian156@gmail.com>
2017-07-25 23:33:50 +0530

    settings privacy settings

--- src/app/main/settings/privacySettings/privacySettings.controller.js
+++ src/app/main/settings/privacySettings/privacySettings.controller.js
@@ -6,8 +6,35 @@
         .controller('PrivacySettingsController', PrivacySettingsController);
 
     /** @ngInject */
-    function PrivacySettingsController() {
+    function PrivacySettingsController($scope) {
         var vm = this;
+        $scope.phonePrivacy = false;
+        $scope.photoPrivacy = false;
+        $scope.visitorSettings = false;
+        $scope.shortlistSettings = false;
+        $scope.donotDisturb = false;
+        $scope.profilePrivacy = false;
+        $scope.openPhonePrivacy = function() {
+            $scope.phonePrivacy = !$scope.phonePrivacy;
+        }
+        $scope.openPhotoPrivacy = function() {
+
+            $scope.photoPrivacy = !$scope.photoPrivacy;
+        }
+        $scope.openVisitorSettings = function() {
+
+            $scope.visitorSettings = !$scope.visitorSettings;
+        }
+        $scope.openShortlistSettings = function() {
+
+            $scope.shortlistSettings = !$scope.shortlistSettings;
+        }
+        $scope.openDonotDisturb = function() {
+            $scope.donotDisturb = !$scope.donotDisturb;
+        }
+        $scope.openProfilePrivacy = function() {
+            $scope.profilePrivacy = !$scope.profilePrivacy;
+        }
         console.log("PrivacySettingsController");
 
     }
