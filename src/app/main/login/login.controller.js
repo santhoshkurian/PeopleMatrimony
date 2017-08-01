88b837369a2814ad9c1f8bd9787940a088b97caa
santhosh kurian <santhoshkurian156@gmail.com>
2017-08-01 21:55:45 +0530

    backend integration

--- src/app/main/login/login.controller.js
+++ src/app/main/login/login.controller.js
@@ -6,9 +6,13 @@
         .controller('LoginController', LoginController);
 
     /** @ngInject */
-    function LoginController($uibModal,$state) {
+    function LoginController($uibModal,$state,$http,$scope) {
         //var vm = this;
         console.log("LoginController");
+
+        $scope.reg={
+
+        };
 
         var vm = this;
         vm.open;
@@ -42,9 +46,23 @@
 
 
         vm.registerCandidate = function () {
-            console.log("efgegf")
-            $state.go('reg');
-          //  $uibModalInstance.close(vm.selected.item);
+            console.log($scope.reg);
+            $http({
+                method: 'POST',
+                url: 'https://devapi.peoplematrimony.com/user?step=1&profile_for='+$scope.reg.profile_for+'&' +
+                'name='+$scope.reg.name+'&gender='+$scope.reg.gender+'&dob='+$scope.reg.dob+'&religion='+parseInt($scope.reg.religion)+'&mothertongue=5&' +
+                'country_code='+$scope.reg+'&email='+$scope.reg.email+'&' +
+                'mobile='+$scope.reg.mobile+'&password='+$scope.reg.password+'&source=111&src=fb&fb_id=test'
+            }).then(function successCallback(response) {
+                console.log(response);
+                // this callback will be called asynchronously
+                // when the response is available
+            }, function errorCallback(response) {
+                console.log(response);
+
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
         };
 
 
