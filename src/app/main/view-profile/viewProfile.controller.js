d7c818f61ce1ad0eb2316c508b166a5689095c2d
santhosh kurian <santhoshkurian156@gmail.com>
2017-08-06 21:15:58 +0530

    view profile api added

--- src/app/main/view-profile/viewProfile.controller.js
+++ src/app/main/view-profile/viewProfile.controller.js
@@ -6,12 +6,26 @@
         .controller('ViewProfileController', ViewProfileController);
 
     /** @ngInject */
-    function ViewProfileController($scope) {
+    function ViewProfileController($scope,$http,storageService) {
 
         $scope.viewType = 'personal';
         $scope.selectType = selectType;
         function selectType(type){
             $scope.viewType = type;
         }
+
+        $http({
+            method: 'GET',
+            url: 'https://devapi.peoplematrimony.com/user/view?'+
+            'view_id='+storageService.get("id")+'&token='+storageService.get("token")
+        }).then(function successCallback(response) {
+           console.log(response)
+        }, function errorCallback(response) {
+            console.log(response)
+
+        });
+
+
+
     }
 })();
