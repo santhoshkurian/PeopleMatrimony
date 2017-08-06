9af46e55bad9b9ebe3eee0359271267a1402ca60
santhosh kurian <santhoshkurian156@gmail.com>
2017-08-06 20:49:44 +0530

    logout api added

--- src/app/toolbar/dashboard/dbtoolbar.controller.js
+++ src/app/toolbar/dashboard/dbtoolbar.controller.js
@@ -7,13 +7,29 @@
         .controller('DashboardToolbarController', DashboardToolbarController);
 
     /** @ngInject */
-    function DashboardToolbarController($scope,$location)
+    function DashboardToolbarController($http,$scope,$location,storageService,$state)
     {
 
         console.log("DashboardToolbarController")
+        $scope.logout = logout;
 
         $scope.isActive = function (viewLocation) {
             return viewLocation === $location.path();
         };
+
+        function logout(){
+            $http({
+                method: 'GET',
+                url: 'https://devapi.peoplematrimony.com/user/logout?'+
+                'id_people='+storageService.get("id")+'&token='+storageService.get("token")
+            }).then(function successCallback(response) {
+                storageService.set("token",null);
+                storageService.set("id",null);
+                $state.go('login');
+
+            }, function errorCallback(response) {
+
+            });
+        }
     }
 })();
