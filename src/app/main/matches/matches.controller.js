7390101b035796c61a521a3a29f1939bee625ebc
santhosh kurian <santhoshkurian156@gmail.com>
2017-08-26 21:04:31 +0530

    matches api added

--- src/app/main/matches/matches.controller.js
+++ src/app/main/matches/matches.controller.js
@@ -6,14 +6,28 @@
         .controller('MatchesController', MatchesController);
 
     /** @ngInject */
-    function MatchesController($scope) {
+    function MatchesController($scope,storageService,$http) {
         var vm = this;
-        console.log("MatchesController");
+        $scope.matches= [];
         $scope.currentPage = 1;
         $scope.totalPAges = 20;
         $scope.pageChange = function () {
           console.log($scope.currentPage)
         }
+
+
+
+        $http({
+            method: 'GET',
+            url: 'https://devapi.peoplematrimony.com/matches/new?' +
+            '&token=' + storageService.get("token")
+        }).then(function successCallback(response) {
+            console.log(JSON.stringify(response))
+            $scope.matches = response.data.matches;
+        }, function errorCallback(response) {
+            console.log(response)
+
+        });
 
     }
 
