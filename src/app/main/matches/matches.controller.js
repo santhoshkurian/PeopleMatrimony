488fa40ca51ec1aecbeac743901b5c167deff169
santhosh kurian <santhoshkurian156@gmail.com>
2017-08-26 21:29:53 +0530

    short list api added

--- src/app/main/matches/matches.controller.js
+++ src/app/main/matches/matches.controller.js
@@ -9,12 +9,33 @@
     function MatchesController($scope,storageService,$http) {
         var vm = this;
         $scope.matches= [];
+        $scope.message = null;
         $scope.currentPage = 1;
         $scope.totalPAges = 20;
         $scope.pageChange = function () {
           console.log($scope.currentPage)
         }
 
+        $scope.shortlist = shortlist;
+        function shortlist(id){
+            $http({
+                method: 'GET',
+                url: 'http://devapi.peoplematrimony.com/do/shortlist?' +
+                '&token=' + storageService.get("token")+'&id='+storageService.get('id')+'&view_id='+id
+            }).then(function successCallback(response) {
+               console.log(response)
+                $scope.message = "Successfully Shortlisted";
+
+            }, function errorCallback(response) {
+                console.log(response)
+                if(response.data.message == 'Already exists'){
+                    $scope.message = "Already Shortlisted";
+                }
+
+            });
+
+
+        }
 
 
         $http({
@@ -22,11 +43,10 @@
             url: 'https://devapi.peoplematrimony.com/matches/new?' +
             '&token=' + storageService.get("token")
         }).then(function successCallback(response) {
-            console.log(JSON.stringify(response))
+            console.log(response)
             $scope.matches = response.data.matches;
         }, function errorCallback(response) {
-            console.log(response)
-
+            console.log(response);
         });
 
     }
