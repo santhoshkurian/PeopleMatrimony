1e029066ceea331878ab10c869226cdc4902c74f
santhosh kurian <santhoshkurian156@gmail.com>
2017-09-04 22:01:47 +0530

    changes

--- src/app/main/managephotos/managephoto.controller.js
+++ src/app/main/managephotos/managephoto.controller.js
@@ -6,13 +6,52 @@
         .controller('ManagePhotoController', ManagePhotoController);
 
     /** @ngInject */
-    function ManagePhotoController($scope) {
+    function ManagePhotoController($scope,storageService,$http) {
         console.log("ManagePhotoController");
         $scope.viewType = 'managephoto';
         $scope.selectType = selectType;
         function selectType(type){
             $scope.viewType = type;
         }
+
+        $scope.uploadFile = function () {
+            var file = document.getElementById("myFile");
+            console.log(file.files[0]);
+            var k = "https://devapi.peoplematrimony.com/image/upload";
+           var  j = new FormData;
+            j.append("id", storageService.get("id")),
+                j.append("token", storageService.get("token")),
+                j.append("image",file.files[0]);
+
+
+                $http({
+                    method: "post",
+                    url: "https://devapi.peoplematrimony.com/image/upload",
+                        headers: {"Content-Type": void 0},
+                        transformRequest: angular.identity,
+                    data : j
+                }).then(function successCallback(response) {
+                    console.log(response)
+                }, function errorCallback(response) {
+                    console.log(response);
+                });
+
+
+
+            //$http.post(k, j, {
+            //    headers: {"Content-Type": void 0},
+            //    transformRequest: angular.identity
+            //}).success(function (a) {
+            //    console.log(a)
+            //}).error(function (a, b) {
+            //    console.log(a,b)
+            //
+            //});
+
+
+
+        };
+
 
 
         $scope.images = [
