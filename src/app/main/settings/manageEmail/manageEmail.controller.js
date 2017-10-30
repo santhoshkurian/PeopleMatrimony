04fc962de87413c551362776271d32c14f291d30
santhosh kurian <santhoshkurian156@gmail.com>
2017-10-30 19:44:41 +0530

    manage alert added

--- src/app/main/settings/manageEmail/manageEmail.controller.js
+++ src/app/main/settings/manageEmail/manageEmail.controller.js
@@ -6,10 +6,30 @@
         .controller('ManageEmailController', ManageEmailController);
 
     /** @ngInject */
-    function ManageEmailController(alert,$scope) {
+    function ManageEmailController(alert,$scope,$http,storageService) {
         var vm = this;
         console.log(alert);
         $scope.alert = alert;
+        $scope.updateAlert = updateAlert;
+
+        function updateAlert(obj){
+            console.log($scope.alert);
+                $scope.passwordMsg= null;
+
+                $http({
+                    method: 'POST',
+                    url: 'http://devapi.peoplematrimony.com/settings/email?' +
+                    'token=' +storageService.get('token')+
+                    '&id=' +storageService.get('id')+
+                    '&'+obj+'='+$scope.alert[obj]
+                }).then(function successCallback(response) {
+                }, function errorCallback(response) {
+
+
+                });
+            }
+
+
 
 
     }
