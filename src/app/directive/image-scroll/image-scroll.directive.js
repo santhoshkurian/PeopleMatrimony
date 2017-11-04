a2a444887230818a964cbfadf30dff58ef379563
santhosh kurian <santhoshkurian156@gmail.com>
2017-11-04 11:54:48 +0530

    changess

--- src/app/directive/image-scroll/image-scroll.directive.js
+++ src/app/directive/image-scroll/image-scroll.directive.js
@@ -7,14 +7,28 @@
             return {
                 restrict: 'E',
                 scope: {
-                    user: '='
+                    user: '=',
+                    gender: '='
                 },
                 templateUrl: '/app/directive/image-scroll/image-scroll.html',
                 controller: function ($scope) {
-                    $scope.display = $scope.user[0].image;
-                    $scope.pos = 0;
+                    $scope.display ='';
                     $scope.size = $scope.user.length;
-                    console.log($scope.size)
+                    if($scope.size != 0) {
+                        $scope.display = $scope.user[0].image;
+                        $scope.pos = 0;
+                    }
+
+                    if($scope.size == 0) {
+                        if($scope.gender == 'female'){
+                            $scope.display = "/assets/defaultImages/female_card.jpg";
+                        }
+                        if($scope.gender == 'male'){
+                            $scope.display = "/assets/defaultImages/male_card.jpg";
+                        }
+                        $scope.pos = 0;
+                    }
+
                     $scope.imageForward = function () {
                         if($scope.pos < $scope.size-1) {
                             $scope.pos = $scope.pos +1;
