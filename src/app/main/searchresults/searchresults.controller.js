3b86ac5c6e4e1406f6f2ca5b802ff69e67de814a
santhosh kurian <santhoshkurian156@gmail.com>
2017-11-26 10:38:48 +0530

    changes

--- src/app/main/searchresults/searchresults.controller.js
+++ src/app/main/searchresults/searchresults.controller.js
@@ -19,7 +19,11 @@
             $http({
                 method: 'POST',
                 url: resourceUrl.url() + 'matches?' +
-                '&token=' + storageService.get("token") + '&type=search&age_start=' + $scope.regular.age_start + '&age_end=' + $scope.regular.age_end
+                '&token=' + storageService.get("token") + '&type=search&' +
+                'age_start=' + $scope.regular.age_start +
+                '&height_start=' + $scope.regular.height_start +
+                '&height_end=' + $scope.regular.height_end + '' +
+                '&age_end=' + $scope.regular.age_end
             }).then(function successCallback(response) {
                 console.log(response)
                 $scope.matches = response.data.matches;
