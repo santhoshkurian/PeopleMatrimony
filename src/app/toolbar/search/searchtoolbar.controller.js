775b361a250b0bfec736262a3af22b0ef08298a4
santhosh kurian <santhoshkurian156@gmail.com>
2017-12-10 21:40:59 +0530

    changes

--- src/app/toolbar/search/searchtoolbar.controller.js
+++ src/app/toolbar/search/searchtoolbar.controller.js
@@ -16,6 +16,10 @@
 
 
         $scope.isActive = function (viewLocation) {
+            var ul = $location.path().split('/');
+            if(ul.length > 3){
+                viewLocation = viewLocation+"/"+ul[3];
+            }
             return viewLocation === $location.path();
         };
     }
