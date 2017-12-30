1bd0633626147b08d6fe4ffafb3562ac426f49df
santhosh kurian <santhoshkurian156@gmail.com>
2017-12-30 10:26:25 +0530

    changes

--- src/app/main/registration2/registrationStep2.controller.js
+++ src/app/main/registration2/registrationStep2.controller.js
@@ -31,30 +31,35 @@
         };
 
         $scope.pinVerify = function(){
+            if($scope.userPin != null && $scope.userPin != '') {
+                $http({
+                    method: 'POST',
+                    url: resourceUrl.url() + 'user?step=3&' +
+                    'id_people=' + $scope.memberId + '&' +
+                    'token=' + storageService.get('token') +
+                    '&user_pin=' + $scope.userPin
+                }).then(function successCallback(response) {
+                    console.log(response);
+                    storageService.set("token", '');
+                    storageService.set("id", '');
+                    storageService.set("image_url", '');
+                    storageService.set("name", '');
+                    storageService.set("package", '');
+                    storageService.set("regular_search", '');
+                    $state.go('login')
+                }, function errorCallback(response) {
+                    console.log(response)
 
-            $http({
-                method: 'POST',
-                url: resourceUrl.url()+'user?step=3&' +
-                'id_people='+$scope.memberId+'&' +
-                'token='+storageService.get('token')+
-                    '&user_pin='+$scope.userPin
-            }).then(function successCallback(response) {
-                console.log(response);
-                storageService.set("token",'');
-                storageService.set("id",'');
-                storageService.set("image_url",'');
-                storageService.set("name",'');
-                storageService.set("package",'');
-                storageService.set("regular_search",'');
-                $state.go('login')
-            }, function errorCallback(response) {
-                console.log(response)
-
-                $scope.message = "Invalid Pin";
-                $timeout(function() { $scope.message = '';}, 2000);
+                    $scope.message = "Invalid Pin";
+                    $timeout(function () {
+                        $scope.message = '';
+                    }, 2000);
 
 
-            });
+                });
+            }else{
+                $scope.message = 'Invalid PIN';
+            }
 
 
         };
