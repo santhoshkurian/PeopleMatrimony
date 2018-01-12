a658f05dc70c3ebee6e52c38c34cea202ec3a33a
santhosh kurian <santhoshkurian156@gmail.com>
2018-01-12 23:43:38 +0530

    changes

--- src/app/main/dashboard/blocked/blocked.controller.js
+++ src/app/main/dashboard/blocked/blocked.controller.js
@@ -14,5 +14,25 @@
         $scope.name = storageService.get("name");
         $scope.id = storageService.get("id");
         $scope.blocked = blocked;
+
+        $scope.logout = logout;
+        function logout(){
+            $http({
+                method: 'GET',
+                url: resourceUrl.url()+'user/logout?'+
+                'id_people='+storageService.get("id")+'&token='+storageService.get("token")
+            }).then(function successCallback(response) {
+                storageService.set("token",'');
+                storageService.set("id",'');
+                storageService.set("image_url",'');
+                storageService.set("name",'');
+                storageService.set("package",'');
+                storageService.set("regular_search",'');
+                $state.go('login');
+
+            }, function errorCallback(response) {
+
+            });
+        }
     }
 })();
