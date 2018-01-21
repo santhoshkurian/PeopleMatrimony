4b73a21f240314b696da497bc457f6e7c2e40810
santhosh kurian <santhoshkurian156@gmail.com>
2018-01-21 17:01:42 +0530

    changes

--- src/app/directive/image-scroll/image-scroll.directive.js
+++ src/app/directive/image-scroll/image-scroll.directive.js
@@ -9,10 +9,12 @@
                 scope: {
                     images: '=',
                     gender: '=',
+                    settings: '=',
                     reverse: "&"
                 },
                 templateUrl: '/app/directive/image-scroll/image-scroll.html',
                 controller: function ($scope) {
+                    console.log("settings",$scope.settings);
                     $scope.indexValue = 0;
                     $scope.previous = 0;
                     $scope.display ='';
@@ -21,15 +23,37 @@
                         $scope.display = $scope.images[0].image;
                         $scope.pos = 0;
                     }
-                    if($scope.size == 0) {
-                        console.log("check",$scope.gender);
-                        if($scope.gender == 'female'){
-                            $scope.images.push({image:"/assets/defaultImages/female_card.jpg"});
+                    if($scope.settings != null && $scope.settings.length > 0) {
+                        if ($scope.settings[0].photo_privacy == 'protected') {
+                            $scope.images = [];
+                            if ($scope.gender == 'female') {
+                                $scope.images.push({image: "/assets/defaultImages/female_photo_protected_card.jpg"});
+                            }
+                            if ($scope.gender == 'male') {
+                                $scope.images.push({image: "/assets/defaultImages/male_photo_protected_card.jpg"});
+                            }
+                        } else {
+                            if ($scope.size == 0) {
+                                if ($scope.gender == 'female') {
+                                    $scope.images.push({image: "/assets/defaultImages/female_card.jpg"});
+                                }
+                                if ($scope.gender == 'male') {
+                                    $scope.images.push({image: "/assets/defaultImages/male_card.jpg"});
+                                }
+                            }
+
                         }
-                        if($scope.gender == 'male'){
-                            $scope.images.push({image:"/assets/defaultImages/male_card.jpg"});
+                    }else {
+
+                        if ($scope.size == 0) {
+                            if ($scope.gender == 'female') {
+                                $scope.images.push({image: "/assets/defaultImages/female_card.jpg"});
+                            }
+                            if ($scope.gender == 'male') {
+                                $scope.images.push({image: "/assets/defaultImages/male_card.jpg"});
+                            }
+                            $scope.pos = 0;
                         }
-                        $scope.pos = 0;
                     }
 
                     $scope.imageForward = function () {
